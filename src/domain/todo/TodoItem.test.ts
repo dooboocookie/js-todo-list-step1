8ca1bae78871a896aad57141154f3cfd4839736e
luca <bkh3030@gmail.com>
2024-01-09 16:48:58 +0900

    feat: ToDoItem 투두 리스트의 목록 도메인 모델 생성 및 테스트

---
+++ src/domain/todo/TodoItem.test.ts
@@ -0,0 +1,60 @@
+import { ToDoItem } from "./TodoItem";
+
+describe("TodoItem", () => {
+  it("내용과 완료 여부를 입력 받아 생성한다", () => {
+    //given
+    const inputContent = "내용입니다";
+    const inputCompleted = false;
+    //when
+    const actual = new ToDoItem(inputContent, inputCompleted);
+    //then
+    expect(actual.content).toBe(inputContent);
+  });
+
+  it("내용이 없으면 예외를 발생한다", () => {
+    //given
+    const inputContent = "";
+    const inputCompleted = false;
+    //when & then
+    expect(() => {
+      new ToDoItem(inputContent, inputCompleted);
+    }).toThrow(new Error("값이 존재하지 않습니다."));
+  });
+
+  it("완료 상태를 변경한다", () => {
+    //given
+    const inputContent = "내용입니다";
+    const inputCompleted = false;
+    const toDoItem = new ToDoItem(inputContent, inputCompleted);
+    //when
+    toDoItem.switchCompleted();
+    const actual = toDoItem.isCompleted;
+    //then
+    expect(actual).toBe(true);
+  });
+
+  it("내용을 변경한다", () => {
+    //given
+    const inputContent = "내용입니다";
+    const inputCompleted = false;
+    const toDoItem = new ToDoItem(inputContent, inputCompleted);
+    const expected = "변경된 내용입니다";
+    //when
+    toDoItem.edit(expected);
+    const actual = toDoItem.content;
+    //then
+    expect(actual).toBe(expected);
+  });
+
+  it("없는 값으로는 내용 변경이 불가합니다.", () => {
+    //given
+    const inputContent = "내용입니다";
+    const inputCompleted = false;
+    const toDoItem = new ToDoItem(inputContent, inputCompleted);
+    const empty = "";
+    //when & then
+    expect(() => {
+      toDoItem.edit(empty);
+    }).toThrow(new Error("값이 존재하지 않습니다."));
+  });
+});
--- src/domain/todo/TodoItem.ts
+++ src/domain/todo/TodoItem.ts
@@ -1,13 +1,29 @@
 export class ToDoItem {
-  private readonly _content: string;
-  private readonly _isCompleted: boolean;
+  private _content: string;
+  private _isCompleted: boolean;
 
   public constructor(content: string, isCompleted: boolean) {
     if (isCompleted === null) {
       isCompleted = false;
     }
+    this.validateNotEmpty(content);
     this._content = content;
     this._isCompleted = isCompleted;
+  }
+
+  private validateNotEmpty(content: string) {
+    if (content === "" || content === undefined || content === null) {
+      throw new Error("값이 존재하지 않습니다.");
+    }
+  }
+
+  public switchCompleted() {
+    this._isCompleted = !this._isCompleted;
+  }
+
+  public edit(content: string) {
+    this.validateNotEmpty(content);
+    this._content = content;
   }
 
   public get content(): string {
